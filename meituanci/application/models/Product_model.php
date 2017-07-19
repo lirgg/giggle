1c726d84316c2e789bbed0d1266f28250c582c6f
Administrator <1530881498@qq.com>
2017-07-19 16:47:57 +0800

    models->product_model.php

--- meituanci/application/models/Product_model.php
+++ meituanci/application/models/Product_model.php
@@ -1,19 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-/**
- * Created by PhpStorm.
- * User: apple
- * Date: 17/7/14
- * Time: 上午8:58
- */
+
 class Product_model extends CI_Model
 {
     public function get_product(){
         //.....
 //        $sql = 'select * from t_product';
 
-//        $query = $this->db->get('t_product');
-//        return $query->result();
+//        $query = $this->db->get('t_product');   //查询数据库
+//        return $query->result();                  //以数组形式返回查询结果
 
         $sql = "select t_product.*,(select sum(num) num from t_order where t_order.product_id = t_product.product_id) num from t_product";
         return $this->db->query($sql)->result();
