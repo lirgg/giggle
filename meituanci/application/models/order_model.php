70b6313c4e4fb40bf24ad5db14f8e7d817677b99
Administrator <1530881498@qq.com>
2017-07-19 09:52:22 +0800

    models->order_model.php

--- meituanci/application/models/order_model.php
+++ meituanci/application/models/order_model.php
@@ -9,6 +9,24 @@ class Order_model extends CI_Model
         return $this->db->query($sql)->result();
     }
 
+    public function get_count_by_product_id($product_id){
 
+        $sql = "select sum(num) num from t_order where product_id = $product_id";
+        return $this->db->query($sql)->row();
 
+        //        $query = $this->db->get_where('t_order', array('product_id' => $product_id));
+        //        return $query->num_rows();
+    }
+
+    public function add_order($user_id,$product_id,$price,$num){
+        $data = array(
+            'user_id'=>$user_id,
+            'product_id'=>$product_id,
+            'order_price'=>$price,
+            'num'=>$num
+        );
+
+        $this->db->insert('t_order',$data);
+        return $this->db->affected_rows();
+    }
 }
